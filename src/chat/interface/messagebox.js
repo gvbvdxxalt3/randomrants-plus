28f5394169b4de241a1ce4bcc8374a18d1a36e5d
gvbvdxxalt3 <jasonglenevans2010@outlook.com>
2025-08-26 12:06:35 -0400

    Added private messaging.

--- src/chat/interface/messagebox.js
+++ src/chat/interface/messagebox.js
@@ -10,57 +10,85 @@ var myChatHistory = [];
 var chatHistoryNumber = 0;
 
 function sendMessageFromTextBox() {
-  var message = messageInputBox.value;
-  if (message.trim().length < 1) {
-    return;
-  }
-  sws.send(
-    JSON.stringify({
-      type: "postMessage",
-      message,
-    })
-  );
-  myChatHistory.push(message); // Add to chat history.
-  myChatHistory = myChatHistory.slice(-100); // Keep only the last 100 messages to try to avoid memory overflow.
-  chatHistoryNumber = myChatHistory.length; // Reset to latest message position.
+	var message = messageInputBox.value;
+	var messageTrimmed = message.trim();
+	if (messageTrimmed.length < 1) {
+		return;
+	}
+	if (messageTrimmed.startsWith("@")) {
+		var splitMessage = messageTrimmed.split(" ");
+		var targetUsername = splitMessage[0].slice(1); //Remove the @ symbol.
+		var privateMessage = splitMessage.slice(1).join(" ");
+		
+		if (!targetUsername) {
+            dialogs.alert("You need to type a username, example: @someone hello!");
+            return;
+		}
+        if (privateMessage) {
+            return;
+        }
+        if (privateMessage.trim().length < 1) {
+            return;
+        }
+		
+		sws.send(
+    		JSON.stringify({
+    			type: "postMessagePrivate",
+    			message: privatemessage,
+                targetUser: targetUsername
+    		}),
+    	);
+		
+		return;
+	}
+	
+	sws.send(
+		JSON.stringify({
+			type: "postMessage",
+			message,
+		}),
+	);
+	myChatHistory.push(message); // Add to chat history.
+	myChatHistory = myChatHistory.slice(-100); // Keep only the last 100 messages to try to avoid memory overflow.
+	chatHistoryNumber = myChatHistory.length; // Reset to latest message position.
 }
 
 messageSendButton.addEventListener("click", function () {
-  sendMessageFromTextBox();
-  messageInputBox.value = "";
+	sendMessageFromTextBox();
+	messageInputBox.value = "";
 });
 
 messageInputBox.addEventListener("input", function () {
-  sws.send(
-    JSON.stringify({
-      type: "typing",
-    })
-  );
+	sws.send(
+		JSON.stringify({
+			type: "typing",
+		}),
+	);
 });
 
 messageInputBox.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    sendMessageFromTextBox();
-    messageInputBox.value = "";
-    e.preventDefault();
-    return;
-  }
+	if (e.key === "Enter") {
+		sendMessageFromTextBox();
+		messageInputBox.value = "";
+		e.preventDefault();
+		return;
+	}
 
-  if (e.key === "ArrowUp") {
-    if (chatHistoryNumber > 0) {
-      chatHistoryNumber--;
-      messageInputBox.value = myChatHistory[chatHistoryNumber];
-    }
-    e.preventDefault();
-    return;
-  }
+	if (e.key === "ArrowUp") {
+		if (chatHistoryNumber > 0) {
+			chatHistoryNumber--;
+			messageInputBox.value = myChatHistory[chatHistoryNumber];
+		}
+		e.preventDefault();
+		return;
+	}
 
-  if (e.key === "ArrowDown") {
-    if (chatHistoryNumber < myChatHistory.length) {
-      chatHistoryNumber++;
-      messageInputBox.value = myChatHistory[chatHistoryNumber] || "";
-    }
-    e.preventDefault();
-    return;
-  }
+	if (e.key === "ArrowDown") {
+		if (chatHistoryNumber < myChatHistory.length) {
+			chatHistoryNumber++;
+			messageInputBox.value = myChatHistory[chatHistoryNumber] || "";
+		}
+		e.preventDefault();
+		return;
+	}
 });
